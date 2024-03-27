0e7c37857dc44eda10fd95fe569329909c54149c
Adam Hillo <adam1hillo@gmail.com>
2024-03-27 09:29:27 +0100

    DataSource

---
+++ src/test/java/be/vdab/geld/DataSourceTest.java
@@ -0,0 +1,24 @@
+package be.vdab.geld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+class DataSourceTest {
+    private final DataSource dataSource;
+
+    DataSourceTest(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Test
+    void deDataSourceKanEenConnectionGeven() throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            assertThat(connection.getCatalog()).isEqualTo("geld");
+        }
+    }
+}
